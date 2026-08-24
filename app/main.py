323f651b82a7df860c4be01d8dbc3abe000610f7
Yelyzaveta Hulchenko <liza.gulchenko@gmail.com>
2026-08-24 21:40:14 +0300

    Fixed tests

--- app/main.py
+++ app/main.py
@@ -15,6 +15,6 @@ def create_report(data_file_name: str,
     result = supply - buy
 
     with open(report_file_name, "a") as f:
-        f.write(f"supply,{supply}\n"
-                f"buy,{buy}\n"
-                f"result,{result}")
+        f.write(f"supply,{supply}\n")
+        f.write(f"buy,{buy}\n")
+        f.write(f"result,{result}")
